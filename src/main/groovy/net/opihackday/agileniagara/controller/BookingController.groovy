6a51a936b4a38107781928bf62e0a7b70bd8306b
JonQuarfoth <jon.quarfoth@gmail.com>
2013-11-16 16:08:01 -0600

    fix json

--- src/main/groovy/net/opihackday/agileniagara/controller/BookingController.groovy
+++ src/main/groovy/net/opihackday/agileniagara/controller/BookingController.groovy
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import static groovy.json.JsonOutput.toJson
 
 @RestController
 class BookingController {
@@ -16,23 +17,23 @@ class BookingController {
     RabbitService rabbitService;
 
     @RequestMapping("/api/season")
-    String listSeasons() {
+    List<Map> listSeasons() {
         String email = SecurityContextHolder?.context?.authentication?.principal;
         return rabbitService.getSeasons(email);
     }
 
     @RequestMapping("/api/location")
-    String listLocations() {
+    List<Map> listLocations() {
         return rabbitService.getLocations();
     }
 
     @RequestMapping("/api/booking/location/{locationId}/season/{seasonId}")
-    String listBookings(@PathVariable String locationId, @PathVariable String seasonId) {
+    List<Map> listBookings(@PathVariable String locationId, @PathVariable String seasonId) {
         return rabbitService.getBookings(locationId, seasonId);
     }
 
     @RequestMapping("/api/booking")
-    String createBooking(@RequestParam String locationId, @RequestParam String startDate, @RequestParam String endDate) {
+    Map<String, String> createBooking(@RequestParam String locationId, @RequestParam String startDate, @RequestParam String endDate) {
 
         String email = SecurityContextHolder?.context?.authentication?.principal;
         if (email) {
